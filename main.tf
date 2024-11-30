a2c6229da8f2d62ed5bd94bbadc755ffa0e449c5
pardhasaradhi1919 <43030820+pardhasaradhi1919@users.noreply.github.com>
2024-11-30 12:38:45 +0530

    adding all tf files

---
+++ main.tf
@@ -0,0 +1,32 @@
+#providers
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicopr/aws"
+      version = "4.45.0"
+    }
+  }
+}
+
+#config of azure
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+#create resource 
+# resource "aws_instance" "my_instance" {
+#     ami = "ami-xyddgfgfgfg"
+#     instance_type = "t2.micro"
+#     tags = {
+#         name = "instance1"
+#         org = "xyz"
+#         costcentre = "abc" 
+#     }
+#}
+
+#fetch resource details using data source
+data "aws_instance" "my_instance_meta" {
+  instance_id = var.aws_instance_id
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  default = "us-west-2"
+}
+variable "aws_instance_id" {
+  default = "aws-abcdef1234567"
+}
+variable "aws_access_key" {
+}
+variable "aws_secret_key" {
+}
